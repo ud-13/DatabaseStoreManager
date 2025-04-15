b0030e6e7e26532a2c46a14da170db211b343b50
ud-13 <pratapuday1176@gmail.com>
2025-04-15 21:29:33 +0000

    Restored to 'd72b28eced44f5425c16f8676f5220fc45587d98'
    Replit-Restored-To: d72b28eced44f5425c16f8676f5220fc45587d98

--- Tenant/settings.py
+++ Tenant/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Tenant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('POSTGRES_DB'),
-        'USER': os.environ.get('POSTGRES_USER'),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD'),
-        'HOST': os.environ.get('POSTGRES_HOST'),
-        'PORT': os.environ.get('POSTGRES_PORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
