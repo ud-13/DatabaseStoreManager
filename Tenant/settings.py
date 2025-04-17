5c734ed483fb2f4ecd3b95823851c0deb4690c95
ud-13 <pratapuday1176@gmail.com>
2025-04-17 21:07:01 +0530

    Update settings.py

--- Tenant/settings.py
+++ Tenant/settings.py
@@ -78,12 +78,8 @@ WSGI_APPLICATION = 'Tenant.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('PGDATABASE', ''),
-        'USER': os.environ.get('PGUSER', ''),
-        'PASSWORD': os.environ.get('PGPASSWORD', ''),
-        'HOST': os.environ.get('PGHOST', ''),
-        'PORT': os.environ.get('PGPORT', '5432'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
